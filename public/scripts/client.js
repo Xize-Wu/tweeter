11c2b45877f6042e35d8047fdb5e873ee1bdaec6
Xize Wu <wuxize1995@gmail.com>
2023-04-03 15:28:41 -0400

    responsive design

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,64 +1,56 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-
 $(document).ready(function() {
-  const escape = function (str) {
-    let div = document.createElement("div");
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-  };
-
-  const createTweetElement = function(obj) {
-    let $tweet = `<article class="tweet_article">
-      <header class="user_profile">
-        <div class="avatar" id="avatar"> <img src ="${obj.user.avatars}"></div>
-        <div class="name" id="name">${obj.user.name}</div>
-        <div class="handle" id="handle">${obj.user.handle}</div>
-      </header>
-      <div class="tweet" id="text">${escape(obj.content.text)
-      }</div>
-      <div class="time" id="time">${timeago.format(obj.created_at)}</div>
-      <footer class="iconcollection" id="iconcollection">
-        <div class="icon" id="flag">
-          <i class="fa-regular fa-flag"></i>
-        </div>
-        <div class="icon" id="retweet">
-          <i class="fa-solid fa-retweet"></i>
-        </div>
-        <div class="icon" id="like">
-          <i class="fa-regular fa-heart"></i>
-        </div>
-      </footer>
-    </article>`;
-
-    return $tweet;
-  };
-
-  const renderTweets = function(tweets) {
-    for (const tweet of tweets) {
-      let result = createTweetElement(tweet);
-      $('.tweet-container').append(result);
-    }
-  };
-
-  const loadTweets = function(){
-    $.ajax({
-      type: 'GET',
-      url:'/tweets',
-      data:{get_param: 'value'},
-      // success: function(data){
-      //   console.log(data)
-      // }
-    })
-    .then(function(data){
-      renderTweets(data)
-    }
-      )
-  }
-  
   loadTweets()
 });
+
+const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
+const loadTweets = function(){
+  $.ajax({
+    type: 'GET',
+    url:'/tweets',
+    data:{get_param: 'value'},
+  })
+  .then(function(data){
+    renderTweets(data)
+  }
+    )
+}
+
+const renderTweets = function(tweets) {
+  for (const tweet of tweets) {
+    let result = createTweetElement(tweet);
+    $('.tweet-container').append(result);
+  }
+};
+
+const createTweetElement = function(obj) {
+  let $tweet = `<article class="tweet_article" id='tweet_article'>
+    <header class="user_profile">
+      <div class = "left_column">
+        <div class="avatar" id="avatar"> <img src ="${obj.user.avatars}"></div>
+        <div class="name" id="name">${obj.user.name}</div>
+      </div>
+        <div class="handle" id="handle">${obj.user.handle}</div>
+    </header>
+    <div class="tweet" id="text">${escape(obj.content.text)
+    }</div>
+    <div class="time" id="time">${timeago.format(obj.created_at)}</div>
+    <footer class="iconcollection" id="iconcollection">
+      <div class="icon" id="flag">
+        <i class="fa-regular fa-flag"></i>
+      </div>
+      <div class="icon" id="retweet">
+        <i class="fa-solid fa-retweet"></i>
+      </div>
+      <div class="icon" id="like">
+        <i class="fa-regular fa-heart"></i>
+      </div>
+    </footer>
+  </article>`;
+
+  return $tweet;
+};
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,54 +1,63 @@
-$(document).ready(function() {
-
-  $("#tweet-text").on("input", function() {
-    const count = 140 - $(this).val().length;
-    const counter = $(this).parent().find(".counter");
-    counter.text(count);
-    if (count < 0) {
-      counter.css('color', 'red');
-    } else (
-      counter.css('color', 'black')
-    );
-  });
-
-
-  $(".tweet_article").on("mouseover", function() {
-    $(this).css({
-      'box-shadow': '5px 10px rgba(0, 0, 0, 0.3)'
-    });
-  });
-
-  $(".tweet_article").on("mouseleave", function() {
-    $(this).css({
-      'box-shadow': ''
-    });
-  });
-
-  $(".icon").on("mouseover", function() {
-    $(this).children().css({ "color": "#fad000" });
-  });
-
-  $(".icon").on("mouseleave", function() {
-    $(this).children().css({ "color": "" });
-  });
-
-  //Form Submission using JQuery
+//Form Submission using JQuery
+const errorMsg = function(){
   $(".form-inline").submit(function(event) {
     const data = $(this).serialize();
-    const length = $(this).serialize().length;
-    if (length === 0) {
-      alert("Brevity is the soul of wit, but we need your tweet to have text in it.");
+    const text =$("#tweet-text").val()
+
+    if (text.length === 0) {
+      $(".no-error-msg").replaceWith($("<section class='error-msg'><b>Brevity is the soul of wit, but we need your tweet to have text in it.</b></section>"))
       return false;
-    } else if (length > 140) {
-      alert("Speak less then 140 words, friend, and submit.");
+    } else if (text.length > 140) {
+      $(".no-error-msg").replaceWith($("<section class='error-msg'><b>Speak less, friend, and enter.</b></section>"))
       return false;
     } else {
       $.post("/tweets", data);
       event.preventDefault();
       location.reload()
     }
-
   });
+}
 
+//count input length
+const inputLength = function () {
+  $("#tweet-text").on("input", function() {
+    const count = 140 - $(this).val().length;
+    const $counter = $(this).parent().find(".counter");
+    $counter.text(count);
+    if (count < 0) {
+      $counter.addClass('form--red');
+    } else (
+      $counter.removeClass('form--red')
+    );
+  });
+}
+
+$(document).on("mouseover", ".tweet_article", function(){
+  $(this).css({
+    'box-shadow': '5px 10px rgba(0, 0, 0, 0.3)'
+  });
+})
+
+$(document).on("mouseleave", ".tweet_article", function(){
+  $(this).css({
+    'box-shadow': ''
+  });
+})
+
+$(document).on("mouseover", ".icon", function(){
+  $(this).children().css({ "color": "#fad000" });
+})
+
+$(document).on("mouseleave", ".icon", function(){
+  $(this).children().css({ "color": "" });
+})
+
+$(document).ready(function() {
+  errorMsg();
+  inputLength();
 });
 
+// $(document).on("hover", ".icon", function(){
+//   $(this).children().css({ "color": "#fad000" }),
+//   $(this).children().css({ "color": "" })
+// })
